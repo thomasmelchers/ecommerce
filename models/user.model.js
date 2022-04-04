9ba10473505cb62bb2e193a6b48f3178d53201a1
Thomas Melchers <thomas.melchers@gmail.com>
2022-04-04 11:00:48 +0200

    update user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const validator = require('validator')
 
 const UserSchema = new mongoose.Schema(
     {
@@ -13,13 +13,14 @@ const UserSchema = new mongoose.Schema(
             type: String, 
             required: [true, 'Providing an email is mandatory to register.'],
             unique: [true, 'This email is already taken.'],
+            lowercase: true,
             trim: true,
         },
         password: {
             type: String,
-            trim: true,
-            minLength: 6,
+            minlength: 6,
             required: [true, 'Providing a password is mandatory to register.'],
+            trim: true,
         },
         isAdmin: {
             type: Boolean,
