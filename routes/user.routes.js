a5c1c6e1e11bf346020bd892e2c85d9685c9fae6
Thomas Melchers <thomas.melchers@gmail.com>
2022-04-04 11:02:02 +0200

    modification of user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,19 @@
 const router = require('express').Router()
 const authController = require('../controllers/auth.controller')
 const userController = require('../controllers/user.controller')
+const { verrifyToken, verrifyTokenAndAuthorization, verrifyTokenAndAdmin } = require('../middlewares/verrifyToken')
 
 
 //REGISTRATION
 
 router.post('/register', authController.signUp)
 router.get('/login', authController.signIn)
+
+//USER
+router.patch('/:id', verrifyTokenAndAuthorization, userController.update)
+router.delete('/delete', verrifyTokenAndAuthorization, userController.delete)
+router.get('/:id', verrifyTokenAndAdmin, userController.admin)
+router.get('/', verrifyTokenAndAdmin, userController.allUsers)
+router.get('/statistics', verrifyTokenAndAdmin, userController.stats)
 
 module.exports = router
