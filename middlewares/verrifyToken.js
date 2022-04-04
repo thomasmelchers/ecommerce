4f6af4ee85b7cd416d2d137d88eda4c0784238de
Thomas Melchers <thomas.melchers@gmail.com>
2022-04-04 11:00:10 +0200

    verrifying authorization, token and if the user is an admin

---
+++ middlewares/verrifyToken.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken')
+
+module.exports.verrifyToken = (req, res, next) => {
+
+    const authHeader = req.headers.token 
+    if (authHeader) {
+        const token = authHeader.split(" ")[1]
+        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+            if(err) res.status(403).json('Token us not valid')
+            req.user = user
+            next()
+        })
+    } else {
+        return res.status(401).json('You are not authenticated!')
+    }
+}
+
+module.exports.verrifyTokenAndAuthorization = (req, res, next) => {
+    this.verrifyToken(req, res, ()=> {
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next()
+        } else {
+            res.status(403).json('You are not allowed to do that!')
+        }
+    })
+}
+
+module.exports.verrifyTokenAndAdmin = (req, res, next) => {
+    this.verrifyToken(req, res, ()=> {
+        if(req.user.isAdmin){
+            next()
+        } else {
+            res.status(403).json('You are not allowed to do that!')
+        }
+    })
+}
